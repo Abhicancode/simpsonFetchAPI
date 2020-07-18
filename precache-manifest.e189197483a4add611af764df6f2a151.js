d8d7cd85f7bce8e72b6a81445e5b0dc93fdee6ca
Abhishek Ojha <ojha439@gmail.com>
2020-07-18 10:41:09 +0545

    Updates

---
+++ precache-manifest.e189197483a4add611af764df6f2a151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711af511bb9a06342d460780c0048e96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2532239969862b5b5510",
+    "url": "/static/css/main.eef0c0f5.chunk.css"
+  },
+  {
+    "revision": "f167a6f2783444409a61",
+    "url": "/static/js/2.a5e18397.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5e18397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2532239969862b5b5510",
+    "url": "/static/js/main.15673db4.chunk.js"
+  },
+  {
+    "revision": "40bed3c78a7b449e1946",
+    "url": "/static/js/runtime-main.0327bb69.js"
+  }
+]);
